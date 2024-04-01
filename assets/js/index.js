0fc006409732676acaa9cd2777ba43c6797b5955
CynthiaMichelle9 <129108582+CynthiaMichelle9@users.noreply.github.com>
2024-03-31 23:57:34 -0300

    formulario de contacto

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,8 @@
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+import '../../node_modules/bootstrap/js/dist/tooltip.js';
+import '../../node_modules/bootstrap/js/dist/modal.js';
+
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
