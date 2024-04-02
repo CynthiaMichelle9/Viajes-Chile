45b3a4ff012ec760b727131e2f8b4f52e0a8feb0
CynthiaMichelle9 <129108582+CynthiaMichelle9@users.noreply.github.com>
2024-04-02 13:37:57 -0300

    importacion js

--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
-import '../../node_modules/bootstrap/js/dist/tooltip.js';
-import '../../node_modules/bootstrap/js/dist/modal.js';
-
-
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();
-});
---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+import '../../node_modules/bootstrap/js/dist/tooltip.js';
+
+
+/* Activar los tooltips */
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
